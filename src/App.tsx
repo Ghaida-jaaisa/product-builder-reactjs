9fb7334679cbb7579528219ebde112d75d384292
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2025-12-31 06:21:16 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
+// import { Button } from "@headlessui/react";
 import { ProductCard } from "./components/ProductCard";
+import Modal from "./components/ui/Modal";
+import Button from "./components/ui/Button";
 import { productList } from "./data";
+import { useState } from "react";
 
 function App() {
-  // ** Renders
+  //  State
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Handler
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
+  //  Renders
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} proudct={product} />
   ));
   return (
     <>
       <main className="container mx-auto">
+        <Button
+         className="bg-indigo-700 hover:bg-indigo-800"
+         onClick={open}>Add</Button>
         <div className="m-5 grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-md">
           {renderProductList}
         </div>
+        <Modal title="Add New Product" isOpen={isOpen} closeModal={close}>
+          <div className="flex items-center space-x-3 ">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Submit
+            </Button>
+            <Button className="bg-gray-300 hover:bg-gray-800">Cancel</Button>
+          </div>
+        </Modal>
       </main>
     </>
   );
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,60 @@
+import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { type ReactNode } from "react";
+
+interface IProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  title?: string;
+  children: ReactNode;
+}
+
+export default function Modal({ isOpen, closeModal, title, children }: IProps) {
+  //   const [isOpen, setIsOpen] = useState(true)
+  //   function open() {
+  //     setIsOpen(true)
+  //   }
+
+  //   function close() {
+  //     setIsOpen(false)
+  //   }
+
+  return (
+    <div>
+      <Button
+        onClick={open}
+        className="rounded-md bg-black/50 px-4 py-2 text-sm font-medium text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-black/30"
+      >
+        Open dialog
+      </Button>
+
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={close}
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white/50 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              {title && (
+                <DialogTitle
+                  as="h3"
+                  className="text-base/7 font-medium text-white"
+                >
+                  Payment successful
+                </DialogTitle>
+              )}
+
+              <div className="mt-4">
+
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  );
+}
